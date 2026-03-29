1714c125b3a8b1d85dc058a9af6d9118da3d7c02
JeovanyC <jeovanyc314@gmail.com>
2026-03-29 18:12:44 -0300

    solved

---
+++ Think Java/Ex(s) 3/CelsiusFahr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CelsiusFahr {
+
+    public static void main (String[] args) {
+        if (args.length != 1) {
+	  System.out.println("Invalid input: only one argument is accepted (Celsius)");
+	  return;
+        }
+        
+        double celsius = Double.parseDouble(args[0]);
+        double fahr = celsius * 9/5 + 32;
+
+        System.out.printf("%.1f C = %.1f F%n", celsius, fahr);
+    }
+}
