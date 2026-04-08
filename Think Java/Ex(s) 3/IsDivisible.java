855768c129b34f4bcc194d061fa72ed24ac450f6
JeovanyC <jeovanyc314@gmail.com>
2026-04-07 21:21:48 -0300

    Solved

---
+++ Think Java/Ex(s) 3/IsDivisible.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IsDivisible{
+
+    public static void main(String[] args) {
+        if (args.length() != 2) {
+	  System.out.println("Invalid input value: provide two integer");
+	  return;
+        }
+        
+        Scanner in = new Scanner(in);
+
+        int n;
+        int m;
+        try {
+	  n = Integer.parseInteger(args[0]);
+	  m = Integer.parseInteger(args[1]);
+        } catch (NumberFormatException err) {
+	  System.out.println("Invalid input value: provide two integer");
+	  return;
+        }
+
+        if (m == 0) {
+	  System.out.println("Division by zero is not allowed");
+	  return;
+        }
+
+        System.out.println(n % m == 0);
+        in.close()
+    }
+}
